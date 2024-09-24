b95409d6dcd9093f91bef343d377974b92f9705d
Junior36912 <juniojackson369@gmail.com>
2024-09-24 16:37:09 -0300

    Adicionando livros.js

---
+++ livros.js
@@ -0,0 +1,13 @@
+// livro.js
+const livro = {
+    titulo: "A Biblioteca da meia noite",
+    autor: "Matt Haig",
+    anoPublicacao: 2021,
+    genero: "Distopia"
+  };
+  
+  console.log(`Título: ${livro.titulo}`);
+  console.log(`Autor: ${livro.autor}`);
+  console.log(`Ano de Publicação: ${livro.anoPublicacao}`);
+  console.log(`Gênero: ${livro.genero}`);
+  
