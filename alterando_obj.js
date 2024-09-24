0d96d01328d98d2b8a7ec220e4f24c63fc37a91e
Junior36912 <juniojackson369@gmail.com>
2024-09-24 15:56:34 -0300

    Add Alterando OBJ

---
+++ alterando_obj.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+  }
+  
+  estudante.telefone = '551199999999';
+  
+  console.log(estudante.telefone);
