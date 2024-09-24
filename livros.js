05702873eecf2dff4769644aa9ba362569ce75ef
Junior36912 <juniojackson369@gmail.com>
2024-09-24 16:40:04 -0300

    Atualizando Livros.js: Calculando a idade de publicação e detalhes

--- livros.js
+++ livros.js
@@ -4,10 +4,18 @@ const livro = {
     autor: "Matt Haig",
     anoPublicacao: 2021,
     genero: "Distopia"
-  };
-  
-  console.log(`Título: ${livro.titulo}`);
-  console.log(`Autor: ${livro.autor}`);
-  console.log(`Ano de Publicação: ${livro.anoPublicacao}`);
-  console.log(`Gênero: ${livro.genero}`);
-  
+};
+
+anoAtual = 2024
+
+livro.idadePublicacao = anoAtual - livro.anoPublicacao;
+
+const mostrarDetalhes = `
+Título: ${livro.titulo}, 
+Autor: ${livro.autor}, 
+Ano de Publicação: ${livro.anoPublicacao}, 
+Gênero: ${livro.genero}, 
+Idade da Publicação: ${livro.idadePublicacao} anos.
+`;
+
+console.log(mostrarDetalhes);
