a8e0d541d05f416a0406be5c05a68d8d4acbcf07
Junior36912 <juniojackson369@gmail.com>
2024-09-24 17:20:07 -0300

    Add Aninhando.js

---
+++ aninhando_obj.js
@@ -0,0 +1,34 @@
+console.log("-----------Estudante 1-----------------")
+
+const estudante = {
+    nome: 'Júnior Jackson',
+    idade: 19,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: false,
+    telefones: ['551199999998', '551199999993'],
+}
+
+estudante.endereco = {
+    rua: 'Rua Muito Engraçada',
+    numero: '51',
+    complemento: 'apto 8'
+}
+
+console.log(estudante);
+
+console.log("-----------Estudante 2-----------------")
+
+const estudante2 = {
+    nome: 'João Marcos',
+    idade: 22,
+    cpf: '32132132132',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['930444444444', '4353545676666'],
+    rua: 'Rua Pescado-Grande',
+    numero: '24',
+    complemento: 'Casa'
+}
+
+console.log(estudante2);
