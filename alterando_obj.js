65ec9ce2cd82883a8ef858e58120a4da9b99f133
Junior36912 <juniojackson369@gmail.com>
2024-09-24 15:59:52 -0300

    Atualizando o Alterando OBJ

--- alterando_obj.js
+++ alterando_obj.js
@@ -2,9 +2,17 @@ const estudante = {
     nome: 'José Silva',
     idade: 32,
     cpf: '12312312312',
-    turma: 'JavaScript'
-  }
-  
-  estudante.telefone = '551199999999';
-  
-  console.log(estudante.telefone);
+turma: 'JavaScript'
+}
+
+estudante.telefone = '551199999999';
+
+console.log("Antes de add telefone: ", estudante)
+
+console.log("---------------------------------------------")
+
+console.log("Adicionando telefone: ", estudante.telefone);
+
+console.log("---------------------------------------------")
+
+console.log("Depois de add telefone: ", estudante)
