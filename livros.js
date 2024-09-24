c2ade4ebba9b28cb03479c03da34d5a27ac283a8
Junior36912 <juniojackson369@gmail.com>
2024-09-24 16:44:36 -0300

    Atualizando Livros.js: Livro 2 - Os 7 Maridos de Evellyn Hugo (em colchetes)

--- livros.js
+++ livros.js
@@ -18,4 +18,25 @@ Gênero: ${livro.genero},
 Idade da Publicação: ${livro.idadePublicacao} anos.
 `;
 
+console.log("----------------------------------------------")
+
 console.log(mostrarDetalhes);
+
+console.log("----------------------------------------------")
+
+const livro2 = {
+    titulo: "Os sete maridos de Evelyn Hugo",
+    autor: "TAYLOR JENKINS REID",
+    anoPublicacao: 2019,
+    genero: "Romance",
+    idadePublicacao: new Date().getFullYear() - 2019
+};
+
+console.log(`Título: ${livro2["titulo"]}`);
+console.log(`Autor: ${livro2["autor"]}`);
+console.log(`Ano de Publicação: ${livro2["anoPublicacao"]}`);
+console.log(`Gênero: ${livro2["genero"]}`);
+console.log(`Idade de Publicação: ${livro2["idadePublicacao"]} anos`);
+  
+console.log("----------------------------------------------")
+
