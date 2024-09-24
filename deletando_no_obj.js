0c4a66e14010263ce0f999c086437e549505c560
Junior36912 <juniojackson369@gmail.com>
2024-09-24 16:09:20 -0300

    Add Deletando Info do OBJ

---
+++ deletando_no_obj.js
@@ -0,0 +1,12 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+}
+
+console.log(objPersonagem)
