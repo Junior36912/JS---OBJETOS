69e8c9b1523167a6e748aeb99caf66ad1b143f94
Junior36912 <juniojackson369@gmail.com>
2024-09-25 00:25:23 -0300

    Atualização do objCalculadora.js - Adicionado o calcular média

--- objCalculadora.js
+++ objCalculadora.js
@@ -12,12 +12,29 @@ const Calculadora = {
         return total;
     },
     Divisao: function(valor1, valor2) {
-        if (valor1 == 0 || valor2 == 0) {
+        if (valor2 === 0) {
             return console.log("Existe 0 na operação!");
         } else {
             const total = valor1 / valor2;
             return total;
         }
+    },
+    Media: function(lista) {
+        const tamanho = lista.length;
+        
+        if (tamanho === 0) {
+            return "Erro: Lista vazia.";
+        }
+
+        let somaTotal = 0;
+
+        lista.forEach(item => {
+            somaTotal += item;
+        });
+
+        const media = somaTotal / tamanho;
+
+        return media;
     }
 }
 
@@ -25,3 +42,7 @@ console.log("Soma:", Calculadora.Soma(3, 1));
 console.log("Subtração:", Calculadora.Subtracao(3, 1));
 console.log("Multiplicação:", Calculadora.Multiplicacao(3, 1));
 console.log("Divisão:", Calculadora.Divisao(3, 1));
+
+const l1 = [10, 9, 8, 7];
+
+console.log(Calculadora.Media(l1))
