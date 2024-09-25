5b859964245daf6ad58c8678ef72ca6d193dbc3c
Junior36912 <juniojackson369@gmail.com>
2024-09-24 23:43:17 -0300

    Atualização da Atividade_1.js: Adicionado Lista Pessoas da qual usa function para visualizar tudo e um filtro por Solteiro

--- atividade_1.js
+++ atividade_1.js
@@ -11,6 +11,8 @@ pessoa.endereco = {
     Estado: "Brasil"
 }
 
+console.log("------------Log Bruto do Objeto - Pessoa-----------------------")
+
 console.log(pessoa)
 
 function mostrarInfoPessoa(pessoa) {
@@ -24,5 +26,39 @@ function mostrarInfoPessoa(pessoa) {
     Estado: ${pessoa.endereco.Estado}   |  |    (${typeof pessoa.endereco})`)
 }
 
+console.log("------------Log Arrumado do Objeto - Pessoa---------------------")
+
 mostrarInfoPessoa(pessoa);
 
+console.log("-----------Log Bruto da Lista - Pessoas----------------------")
+
+const Pessoas = [
+    {nome: "João Marcos", idade: 21, solteiro: false},
+    {nome: "Raiana Santos", idade: 27, solteiro: true},
+    {nome: "Kauã Ferreira", idade: 19, solteiro: true}
+]
+
+console.log(Pessoas)
+
+function mostrarListaPessoas(lista) {
+    lista.forEach(pessoa => {
+        console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Solteiro: ${pessoa.solteiro}`);
+    });
+}
+console.log("------------Log Arrumado da Lista - Pessoas-------------------")
+mostrarListaPessoas(Pessoas);
+
+
+console.log("------------Adicionando na Lista - Pessoas-------------------")
+Pessoas.push({ nome: "Barbara Santos", idade: 19, solteiro: true });
+
+mostrarListaPessoas(Pessoas);
+
+console.log("------------Filtrando Solteiros-------------------");
+
+function filtrarPorSolteiros(lista, solteiro) {
+    return lista.filter(pessoa => pessoa.solteiro === solteiro);
+}
+        
+const solteiros = filtrarPorSolteiros(Pessoas, false);
+mostrarListaPessoas(solteiros);
