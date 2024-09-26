45ddb476893f4d2ab68935aebc4073e1614ab0c0
Junior36912 <juniojackson369@gmail.com>
2024-09-25 22:22:12 -0300

    Atualizando fr_in.js

--- fr_in.js
+++ fr_in.js
@@ -1,22 +1,50 @@
 const estudante = {
-    nome: 'José Silva',
-    idade: 32,
+    nome: 'Junior Jackson',
+    idade: 19,
     cpf: '12312312312',
     turma: 'JavaScript',
-    bolsista: true,
+    bolsista: false,
     telefones: ['551199999998', '551199999993'],
     enderecos: [{
-        rua: 'Rua Joseph Climber',
-        numero: '45',
+        rua: 'Rua Numero-Zero',
+        numero: '69',
         complemento: 'apto 43'
     },
     {
-        rua: 'Rua Dona Clotilde',
+        rua: 'Rua Dos-Bobos',
         numero: '71',
         complemento: null
     }]
 }
 
+console.log("------------------------------------PERCORRENDO CHAVE--------------------------------------")
+
 for (let chave in estudante) {
     console.log(chave);
 }
+
+console.log("----------------------------PERCORRENDO ESTUDANTE PELA CHAVE-------------------------------")
+
+for (let chave in estudante) {
+    console.log(estudante[chave]);
+}
+
+console.log("--------------------------------PERCORRENDO COM O TYPEOF-----------------------------------")
+
+for (let chave in estudante) {
+    const tipo = typeof estudante[chave];
+    const texto = `A chave ${chave} tem o valor de tipo ${estudante[chave]}`
+    console.log(texto);
+}
+
+console.log("--------------------------------PERCORRENDO COM O VALOR-----------------------------------")
+
+for (let chave in estudante) {
+    const tipo = typeof estudante[chave];
+    if (tipo !== 'object' && tipo !== 'function') {
+        const texto = `'${chave}' é: ${estudante[chave]}`
+        console.log(texto);
+    }
+}
+
+
