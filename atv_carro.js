ad6dff4ae2ae0cfa5251d2875cbdee31ec427bad
Junior36912 <juniojackson369@gmail.com>
2024-09-26 01:22:24 -0300

    Add atv_carro.js - Versão Final

--- atv_carro.js
+++ atv_carro.js
@@ -11,12 +11,32 @@
 //Utilize novamente o loop for...in para percorrer todas as propriedades atualizadas do objeto carro e imprima no console o 
 //nome da propriedade e o seu valor.
 
-const objCarro = {
+const objCarro1 = {
     marca: "Citron",
     modelo: "B2",
     ano: 2024,
     cor: "Rosa Barbie"
 }
 
-const infoAntiga = Object.entries(objCarro)
-console.log(infoAntiga)
+console.log("------------------------FUNÇÃO InfoCarro-------------------------")
+
+infoCarro = function(objCarro){
+
+    const valoresObj = Object.entries(objCarro)
+
+    for (const [key] of Object.entries(valoresObj)){
+        console.log(`${valoresObj[key][0]} : ${valoresObj[key][1]}`)
+    };
+
+}
+
+console.log("-----------------------ORIGINAL InfoCarro------------------------")
+
+infoCarro(objCarro1)
+
+objCarro1.roprietario = "Junior Jackson";
+objCarro1.kmRodados = 100;
+
+console.log("-------------FUNÇÃO InfoCarro PÓS ALTERAÇÃO DO OBJ---------------")
+
+infoCarro(objCarro1)
