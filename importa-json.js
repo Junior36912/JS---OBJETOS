be515f5c0a2db734075230a645b334e9622b43ce
Junior36912 <juniojackson369@gmail.com>
2024-09-29 15:40:31 -0300

    Add converte-json.js a atualização de estrutura de outros arquivos

--- importa-json.js
+++ importa-json.js
@@ -1,4 +1,13 @@
-const estudante = require('./estudante.json');
+console.log("---------------ADICIONANDO O ESTUDANTE.JSON PARA UMA VARIAVEL-----------------")
 
+const estudante = require('./estudante.json');
 console.log(estudante);
+
+
+console.log("---------------TIPO DA VARIAVEL ESTUDANTE QUE CONTEM JSON-----------------")
 console.log(typeof estudante)
+
+
+console.log("---------------OBSERVANDO AS CHAVES DO OBJETO-----------------")
+const chaves = Object.keys(estudante);
+console.log(chaves);
